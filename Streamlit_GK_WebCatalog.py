4f830d7aa76c4afb105f6356b0f3eb03037a18d9
Gopal Kumar <jmjpgopal@yahoo.com>
2023-04-25 19:44:19 +0800

    Update Streamlit_GK_WebCatalog.py

--- Streamlit_GK_WebCatalog.py
+++ Streamlit_GK_WebCatalog.py
@@ -1,4 +1,5 @@
 import snowflake.connector
+import streamlit
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(),CURRENT_REGION()")
